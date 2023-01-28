3e1e92bee0b29cc8d5a4b7bb3064d963c2ffc018
xiaoshengMr <xiaoshengmr@163.com>
2023-01-28 18:58:53 +0800

    add lc756

---
+++ UnionFind/Lc756.java
@@ -0,0 +1,51 @@
+package UnionFind;
+
+
+class Lc756 {
+
+    public int minSwapsCouples(int[] row) {
+
+        int len = row.length;
+        int n = len/2;
+        UnionFind find = new UnionFind(n);
+        for(int i =0;i<n;i+=2) {
+            find.union(row[i]/2, row[i+1]/2);
+        }
+        return n -find.getCount();
+    
+    }
+
+    private class UnionFind {
+        private int cnt;
+        private int[] parent;
+
+        public int getCount() {
+            return cnt;
+        }
+        public UnionFind(int n) {
+            this.cnt = n;
+            this.parent = new int[n];
+            for(int i=0;i<n;i++) parent[i]= i;
+        }
+        public int find(int x) {
+            while(x!= parent[x]) {
+                x = parent[x];
+            }
+            return x;
+
+        }
+        public void union (int x , int y) {
+            int rx = find(x);
+            int ry = find(y);
+            if(rx == ry) return;
+            parent[rx] = parent[ry];
+            cnt--;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+
+    }
+    
+}
